5e8c99144bb221b4380e97de6fa2472662c4139c
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-10-04 21:22:35 +0530

    commit

---
+++ Routing/src/App.jsx
@@ -0,0 +1,42 @@
+// import React,{BrouserRouter,Routes,Route, useParams, use} from 'react'
+// import UseLayout from './UseLayout'
+// import UseImperitiveHandle from './UseImperitiveHandle'
+// const App = () => {
+//   function UseImperitiveHandle(){
+//     const id = useParams();
+//     return <h1> user id is : {id.id}</h1>
+//   }
+//   return (
+//     <>
+//     <BrouserRouter>
+//     <Routes>
+//       <Route path='/' element={<UseLayout />} />
+//       <Route path='/useimperative/:id' element={<UseImperitiveHandle />} />
+//        
+//        
+//     </Routes>
+//     </BrouserRouter>
+//     </>
+//   )
+// }
+// export default App
+
+
+// import React from 'react'
+// import {BrowserRouter,Routes,Route,useParams} from 'react-router-dom'
+
+// const App = () => {
+//   function User(){
+//     const {id} = useParams()
+//     return <h2>User is - ID{id}</h2>
+//   }
+//   return (
+//     <BrowserRouter>
+//     <Routes>
+//       <Route path="/user/:id" element={User /}></Route>
+//     </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
---
+++ SkeletonLoading/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState , useEffect } from 'react'
+import Skelton from './skelton'
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
